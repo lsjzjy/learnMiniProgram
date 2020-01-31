bf02ca5413ad367b363929a0153c25b2273d80f6
lsjzjy <lsjazjy957@126.com>
2020-01-31 21:45:57 +0800

    view,image,input的使用

--- pages/input/input.js
+++ pages/input/input.js
@@ -1,66 +1,74 @@
-// pages/button/button.js
+// pages/input/input.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isLoading:true
-  },
 
+  },
+  handleInput(event) {
+    console.log("用户输入内容" , event.detail.value)
+  },
+  handleFous(event) {
+    console.log("用户获取焦点", event)
+  },
+  handleBlur(event){
+    console.log("用户失去焦点",event)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
