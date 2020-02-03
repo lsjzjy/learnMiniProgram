c0b14f94970868829e4bdb3bfdc31a96fb5dfa00
lsjzjy <lsjazjy957@126.com>
2020-02-03 20:41:08 +0800

    登录流程和演练

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+  globalData: {
+    token: ''
+  },
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function() {
+    const token = wx.getStorageSync('token')
+    // 如果有token则进行验证         zZxcv cxz  AQSWEDFGH, 
+    if (token && token.length !== 0) {
+      this.check_toekn(token);
+    } else {
+      this.login();
+    }
+    // code只有5分钟的有效期
+  },
+  check_toekn(token) {
+    console.log('验证token')
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method: "post",
+      header: {
+        token
+      },
+      success: (res) => {
+        if (!res.data.arrCode) {
+          console.log('token有效')
+          this.globalData.token = token;
+        } else {
+          this.login();
         }
+      },
+      fail: (err) => {
+        console.log(err)
       }
     })
   },
-  globalData: {
-    userInfo: null
+  login() {
+    console.log('执行了登录操作')
+    wx.login({
+      success: (res) => {
+        //获取code
+        const code = res.code;
+        //将code发送给我们的服务器
+        wx.request({
+          url: 'http://123.207.32.32:3000/login',
+          method: 'post',
+          data: {
+            code
+          },
+          success: (res) => {
+            const token = res.data.token;
+            this.globalData.token = token;
+            wx.setStorageSync('token', token)
+          }
+        })
+      }
+    })
   }
 })
